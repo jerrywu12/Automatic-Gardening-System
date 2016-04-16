0effeac81f5c8ac06ba8343a00cf816bf4f594bb
Jerry Wu <jerrywu12@gmail.com>
2016-04-16 01:22:39 -0700

    add Sparkfun Luminosity Sensor Breakout - TSL2561

--- JerryAutomaticGardeningSystem.ino
+++ JerryAutomaticGardeningSystem.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 #include <Time.h>
 #include "DHT.h"    // Thermometer+Humidity sensors
+#include <SparkFunTSL2561.h>
+#include <Wire.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -9,6 +11,11 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 #define CHThermo   12   // Connect Digital Pin 
 #define CHLight     2   // Connect Digital Pin  on Arduino to CH3 on Relay Module
 //#define LCDBacklight 13   // TODO: Not able to do it with current sainSmart LCD board, it's using vcc instead of digital pin for power supply
+
+// SFE_TSL2561
+SFE_TSL2561 luxSensor;
+boolean luxGain;                      // Gain setting, 0 = X1, 1 = X16;
+unsigned int luxDelay;  // Integration ("shutter") time in milliseconds
 
 // Thermometer+Humidity
 #define DHTTYPE DHT11   // DHT 11
@@ -77,6 +84,7 @@ String airMenuStr[] = {"ON Duration", "OFF Duration"};
 // Light
 int lightOnTime = 500;   // 5:00
 int lightOffTime = 2200; // 10:00
+double lightSwitchLuxThreshold = 4500;
 
 // Relay Sign
 #define relay_On  0
@@ -119,6 +127,8 @@ void setup() {
   turnOnWaterPump();
   turnOnAirPump();
   turnOnLight();
+
+  setupLightLuxSensor();
 
   dht.begin();
 
@@ -783,17 +793,18 @@ void toggleAirPump()
 void switchLight()
 {
   long currentTime = formatCurrentTime();
-
-Serial.print("current time: ");
-Serial.println(currentTime);
-Serial.print("on time: ");
-Serial.println(lightOnTime);
-Serial.print("off time: ");
-Serial.println(lightOffTime);
-
   // hourOn < hourOff. ex. 0:00 off -> 8:00 on -> 22:00 off
   if (currentTime >= lightOnTime && currentTime < lightOffTime) {
-    turnOnLight();
+
+    double currentEnvironmentLuxValue = getLuxValue();
+    // Save electricity by turn off the light if environment is bright enough
+    if (currentEnvironmentLuxValue > lightSwitchLuxThreshold) {
+      Serial.println("Light saving mode");
+      turnOffLight();
+    }
+    else {
+      turnOnLight();
+    }
   }
   else {
     return turnOffLight();
@@ -895,5 +906,175 @@ String convertTimeToString(long totalms)
 }
 
 
+// Setup Light Lux sensor
+void setupLightLuxSensor()
+{
+  // Initialize the SFE_TSL2561 library
+
+  // You can pass nothing to luxSensor.begin() for the default I2C address (0x39),
+  // or use one of the following presets if you have changed
+  // the ADDR jumper on the board:
+
+  // TSL2561_ADDR_0 address with '0' shorted on board (0x29)
+  // TSL2561_ADDR   default address (0x39)
+  // TSL2561_ADDR_1 address with '1' shorted on board (0x49)
+
+  // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/tsl2561-luminosity-sensor-hookup-guide?_ga=1.70644176.1938687081.1457811508
+
+  luxSensor.begin();
+
+  // Get factory ID from sensor:
+  // (Just for fun, you don't need to do this to operate the sensor)
+
+  unsigned char ID;
+
+  if (luxSensor.getID(ID)) {
+    //    Serial.print("Lux Sensor - factory ID: 0X");
+    //    Serial.print(ID, HEX);
+    //    Serial.println(", should be 0X5X");
+  }
+  // Most library commands will return true if communications was successful,
+  // and false if there was a problem. You can ignore this returned value,
+  // or check whether a command worked correctly and retrieve an error code:
+  else
+  {
+    byte error = luxSensor.getError();
+    printError(error);
+  }
+
+  // The light sensor has a default integration time of 402ms,
+  // and a default gain of low (1X).
+
+  // If you would like to change either of these, you can
+  // do so using the setTiming() command.
+
+  // If luxGain = false (0), device is set to low gain (1X)
+  // If luxGain = high (1), device is set to high gain (16X)
+
+  luxGain = 0;
+
+  // If time = 0, integration will be 13.7ms
+  // If time = 1, integration will be 101ms
+  // If time = 2, integration will be 402ms
+  // If time = 3, use manual start / stop to perform your own integration
+
+  unsigned char time = 2;
+
+  // setTiming() will set the third parameter (ms) to the
+  // requested integration time in ms (this will be useful later):
+
+  luxSensor.setTiming(luxGain, time, luxDelay);
+
+  // To start taking measurements, power up the sensor:
+
+  luxSensor.setPowerUp();
+
+  // The sensor will now gather light during the integration time.
+  // After the specified time, you can retrieve the result from the sensor.
+  // Once a measurement occurs, another integration period will start.
+
+  Serial.print("init ");
+  getLuxValue();
+}
+
+double getLuxValue()
+{
+  // Wait between measurements before retrieving the result
+  // (You can also configure the sensor to issue an interrupt
+  // when measurements are complete)
+
+  // This sketch uses the TSL2561's built-in integration timer.
+  // You can also perform your own manual integration timing
+  // by setting "time" to 3 (manual) in setTiming(),
+  // then performing a manualStart() and a manualStop() as in the below
+  // commented statements:
+
+  //  luxDelay = 1000;
+  // luxSensor.manualStart();
+  delay(luxDelay);
+  // luxSensor.manualStop();
+
+  // Once integration is complete, we'll retrieve the data.
+
+  // There are two light sensors on the device, one for visible light
+  // and one for infrared. Both sensors are needed for lux calculations.
+
+  // Retrieve the data from the device:
+
+  unsigned int data0, data1;
+
+  if (luxSensor.getData(data0, data1))
+  {
+    // getData() returned true, communication was successful
+
+    //    Serial.print("data0: ");
+    //    Serial.print(data0);
+    //    Serial.print(" data1: ");
+    //    Serial.print(data1);
+
+    // To calculate lux, pass all your settings and readings
+    // to the getLux() function.
+
+    // The getLux() function will return 1 if the calculation
+    // was successful, or 0 if one or both of the sensors was
+    // saturated (too much light). If this happens, you can
+    // reduce the integration time and/or gain.
+    // For more information see the hookup guide at: https://learn.sparkfun.com/tutorials/getting-started-with-the-tsl2561-luminosity-sensor
+
+    double lux;    // Resulting lux value
+    boolean good;  // True if neither sensor is saturated
+
+    // Perform lux calculation:
+    good = luxSensor.getLux(luxGain, luxDelay, data0, data1, lux);
+
+    // Print out the results:
+    Serial.print("lux: ");
+    Serial.println(lux);
+
+    if (!good) {
+      Serial.println("WARNING!!! Light lux sensor is not working properly.)");
+    }
+    else {
+      return lux;
+    }
+  }
+  else
+  {
+    Serial.println("WARNING!!! Light lux sensor is not working properly.)");
+    // getData() returned false because of an I2C error, inform the user.
+    byte error = luxSensor.getError();
+    printError(error);
+    return 0.0;
+  }
+}
+
+// If there's an I2C error, this function will print out an explanation.
+void printError(byte error)
+{
+  Serial.print("I2C error: ");
+  Serial.print(error, DEC);
+  Serial.print(", ");
+
+  switch (error)
+  {
+    case 0:
+      Serial.println("success");
+      break;
+    case 1:
+      Serial.println("data too long for transmit buffer");
+      break;
+    case 2:
+      Serial.println("received NACK on address (disconnected?)");
+      break;
+    case 3:
+      Serial.println("received NACK on data");
+      break;
+    case 4:
+      Serial.println("other error");
+      break;
+    default:
+      Serial.println("unknown error");
+  }
+}
 
 
